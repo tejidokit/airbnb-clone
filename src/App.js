4c8bfd783bd13465c2c2f796ef55397c5b648e4f
Kit Tejido <kittejido@gmail.com>
2022-06-08 16:58:21 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import Nav from './components/Nav/Nav.js';
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <Nav/>
     </div>
   );
 }
